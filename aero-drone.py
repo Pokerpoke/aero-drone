2c8f0aa07b53af6cde179f56df8b84774cb74b2c
pokerpoke <j824544269@gmail.com>
2018-09-30 16:07:02 +0800

    test send_ned_velocity

--- aero-drone.py
+++ aero-drone.py
@@ -3,12 +3,30 @@
 
 from dronekit import connect, VehicleMode
 from aerodrone import *
+import math
 
 vehicle = connect("127.0.0.1:14555", wait_ready=True)
 arm_and_take_off(vehicle, 5)
-vehicle.groundspeed = 5 # m/s
-goto(vehicle, 10, 10)
-goto(vehicle, 20, 20)
+vehicle.groundspeed = 5  # m/s
+
+
+# for theta in xrange(360):
+#     x = 5 * math.cos(theta/180*math.pi)
+#     y = 5 * math.sin(theta/180*math.pi)
+#     goto(vehicle, x, y)
+
+v_x = 0.3  # m/s
+v_y = 0.4  # m/s
+
+send_ned_velocity(vehicle, v_x, v_y, 0, 2)
+# goto_position_target_local_ned(vehicle, 5, 5, 0)
+v_x = 2  # m/s
+v_y = -4  # m/s
+
+send_ned_velocity(vehicle, v_x, v_y, 0, 2)
+
+# goto(vehicle, 10, 10)
+# goto(vehicle, 20, 20)
 
 vehicle.mode = VehicleMode("LAND")
 vehicle.close()
--- aerodrone/__init__.py
+++ aerodrone/__init__.py
@@ -1,11 +1,6 @@
 #!/usr/bin/env python
 # -*- coding:utf-8 -*-
 
-__all__ = ["precheck",
-           "distance",
-           "arm_and_take_off",
-           "goto"]
-
 from .arm_and_take_off import *
 from .goto import *
 from .distance import *
