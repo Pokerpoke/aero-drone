ad028e1bbfec05c1b6985abc2f641663b1210f1f
pokerpoke <j824544269@gmail.com>
2018-10-03 21:07:15 +0800

    rotate

--- aero-drone.py
+++ aero-drone.py
@@ -4,29 +4,49 @@
 from dronekit import connect, VehicleMode
 from aerodrone import *
 import math
-
-vehicle = connect("127.0.0.1:14555", wait_ready=True)
-arm_and_take_off(vehicle, 5)
-vehicle.groundspeed = 5  # m/s
+import json
 
 
-# for theta in xrange(360):
-#     x = 5 * math.cos(theta/180*math.pi)
-#     y = 5 * math.sin(theta/180*math.pi)
-#     goto(vehicle, x, y)
+def sgn(num):
+    if num >= 0:
+        return 1
+    else:
+        return -1
 
-v_x = 0.3  # m/s
-v_y = 0.4  # m/s
+#  vehicle = connect("127.0.0.1:14555", wait_ready=True)
+#  arm_and_take_off(vehicle, 5)
+#  vehicle.groundspeed = 1  # m/s
 
-send_ned_velocity(vehicle, v_x, v_y, 0, 2)
-# goto_position_target_local_ned(vehicle, 5, 5, 0)
-v_x = 2  # m/s
-v_y = -4  # m/s
 
-send_ned_velocity(vehicle, v_x, v_y, 0, 2)
+ser = serial.Serial("/dev/serial0", 115200, timeout=0.5)
 
-# goto(vehicle, 10, 10)
-# goto(vehicle, 20, 20)
+while(True):
+    line = ser.readline()
+    try:
+        openmv_data = json.loads(line)
+    except ValueError:
+        continue
+    openmv_cx = openmv_data[u'cx']
+    openmv_cy = openmv_data[u'cy']
+    print(openmv_cx)
+    print(openmv_cy)
+
+    theta = vehicle.heading()
+
+    rotate_theta = (theta + 90) % 360
+
+    dx = openmv_cx-160
+    dy = openmv_cy-120
+
+    dest_x = math.cos(rotate_theta/2/math.pi)*dx - \
+        math.sin(rotate_theta/2/math.pi)*dy
+    dest_y = math.sin(rotate_theta/2/math.pi)*dx + \
+        math.cos(rotate_theta/2/math.pi)*dy
+
+    print(dest_x)
+    print(dest_x)
+
+    #  goto(sgn(dx)*10,
 
 vehicle.mode = VehicleMode("LAND")
 vehicle.close()
