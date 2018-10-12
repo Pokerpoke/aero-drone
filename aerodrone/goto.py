68ef242f53abee73cf9c3bb9f0a4b65fc06e989e
pokerpoke <j824544269@gmail.com>
2018-10-12 09:40:27 +0800

    Update goto

--- aerodrone/goto.py
+++ aerodrone/goto.py
@@ -200,6 +200,22 @@ def send_global_velocity(vehicle, velocity_x, velocity_y, velocity_z, duration):
         time.sleep(1)
 
 
+def send_body_ned_velocity(vehicle, velocity_x, velocity_y, velocity_z, duration=0):
+    msg = vehicle.message_factory.set_position_target_local_ned_encode(
+        0,       # time_boot_ms (not used)
+        0, 0,    # target system, target component
+        # frame Needs to be MAV_FRAME_BODY_NED for forward/back left/right control.
+        mavutil.mavlink.MAV_FRAME_BODY_NED,
+        0b0000111111000111,  # type_mask
+        0, 0, 0,  # x, y, z positions (not used)
+        velocity_x, velocity_y, velocity_z,  # m/s
+        0, 0, 0,  # x, y, z acceleration
+        0, 0)
+    for x in range(0, duration):
+        vehicle.send_mavlink(msg)
+        time.sleep(1)
+
+
 """
 Convenience functions for sending immediate/guided mode commands to control the Copter.
 
